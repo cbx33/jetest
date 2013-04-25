40a8c7f7ec15955b08104865a21d8621163c66d0
Peter Savage <silentkeystroke@googlemail.com>
2013-04-25 22:19:22 +0100

    Testing more stuff

---
+++ genjson.py
@@ -0,0 +1,12 @@
+import json
+import os
+
+obj = {"id":1, "tree":{"p":{}}}
+
+a = os.listdir('p')
+for char in a:
+	obj['tree']['p'][char] = []
+	for post in os.listdir('p' + "/" + char):
+		obj['tree']['p'][char].append({'post':post})
+
+print json.dumps(obj)
